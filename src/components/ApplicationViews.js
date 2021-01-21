96f3efdd09d723f0893ab21160a05f059b24612f
Ni236630 <nicholas.douso@gmail.com>
2021-01-21 15:51:41 -0600

    made animals dynamic; added files for other components

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Route } from "react-router-dom"
 import { Kennel } from "./Kennel"
-import { AnimalCard  } from "./animal/AnimalCard";
 import { LocationCard } from "./Locations/LocationCard";
 import { EmployeeCard } from "./Employees/EmployeeCard";
 import { CustomerCard } from './Customers/CustomerCard';
 import { Home } from "./Home"
+import { AnimalProvider } from './animal/AnimalProvider';
+import { AnimalList } from "./animal/AnimalList";
 
 export const ApplicationViews = () => {
   return (
@@ -16,9 +17,11 @@ export const ApplicationViews = () => {
             </Route>
 
             {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path="/animals">
-                <AnimalCard />
-            </Route>
+            <AnimalProvider>
+                <Route exact path="/animals">
+                    <AnimalList />
+                </Route>    
+            </AnimalProvider>
             
             <Route path="/locations">
                 <LocationCard />
