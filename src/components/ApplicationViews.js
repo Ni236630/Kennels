9c56de7ddb4b75362e9e07488fb14605492b5729
Ni236630 <nicholas.douso@gmail.com>
2021-01-21 20:36:12 -0600

    dynamic customers

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Route } from "react-router-dom"
 import { Kennel } from "./Kennel"
 import { EmployeeCard } from "./Employees/EmployeeCard";
-import { CustomerCard } from './Customers/CustomerCard';
 import { Home } from "./Home"
 import { AnimalProvider } from './animal/AnimalProvider';
 import { AnimalList } from "./animal/AnimalList";
 import { LocationProvider } from "./Locations/LocationProvider";
 import { LocationList } from "./Locations/LocationList";
+import { CustomerProvider } from "./Customers/CustomerProvider";
+import { CustomerList } from "./Customers/CustomerList"
 
 export const ApplicationViews = () => {
   return (
@@ -34,9 +35,11 @@ export const ApplicationViews = () => {
                 <EmployeeCard />
             </Route>
             
-            <Route path="/customers">
-                <CustomerCard />
-            </Route>
+            <CustomerProvider>
+                <Route exact path="/customers">
+                    <CustomerList />
+                </Route>   
+            </CustomerProvider>
             
             <Route path="/kennels">
                 <Kennel />
