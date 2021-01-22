f55ab46cd005b7ef1000d5b0324f383993348f60
Ni236630 <nicholas.douso@gmail.com>
2021-01-22 09:21:28 -0600

    animals display owner and location

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,17 +1,22 @@
 import React, { useContext, useEffect } from "react";
 import { AnimalContext } from "./AnimalProvider";
+import { LocationContext } from "../Locations/LocationProvider";
+import { CustomerContext } from "../Customers/CustomerProvider";
 import { AnimalCard } from "./AnimalCard";
 import "./Animal.css"
+
 
 export const AnimalList = () => {
   // This state changes when `getAnimals()` is invoked below
   const {animals, getAnimals } = useContext(AnimalContext)
-  
+  const {locations, getLocations} = useContext(LocationContext)
+  const {customers, getCustomers} = useContext(CustomerContext)  
   //useEffect - reach out to the world for something
   useEffect(()=>{
     console.log("AnimalList: useEffect - getAnimals")
-    getAnimals()
-    
+    getLocations()
+    .then(getCustomers)
+    . then(getAnimals)
   },[])// eslint-disable-line react-hooks/exhaustive-deps
   
   return (
@@ -19,7 +24,12 @@ export const AnimalList = () => {
       {console.log("AnimalList: Render", animals)}
       {
         animals.map(animal => {
-          return <AnimalCard key={animal.id} animal={animal} />
+          const owner = customers.find(c => c.id === animal.customerId)
+          const clinic = locations.find(l => l.id === animal.locationId)
+          return <AnimalCard key={animal.id} 
+                            animal={animal}
+                            location = {clinic}
+                            customer ={owner} />
         })
       }
     </div>
