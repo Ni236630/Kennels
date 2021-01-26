269620c67558471347a1c3b7afdba43a48a9d688
Ni236630 <nicholas.douso@gmail.com>
2021-01-26 13:57:34 -0600

    refactored locations

--- src/components/Locations/LocationCard.js
+++ src/components/Locations/LocationCard.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./Locations.css"
 
 export const LocationCard = ({location}) => (
   <section className="location">
-    <h3 className="location__name">{location.name}</h3>
-    <div className="location__address">{location.address}</div>
+    <h3 className="location__name">
+      <Link to={`/locations/detail/${location.id}`}>
+        {location.name}
+      </Link>
+      </h3>
+      <div className="location__employees">
+        Employees at location: {location.employees.length}
+      </div>
+      <div className="location__animals">
+        Animals at location: {location.animals.length}
+      </div>
   </section>
 )
---
+++ src/components/Locations/LocationDetail.js
@@ -0,0 +1,46 @@
+import React, { useContext, useState, useEffect } from 'react'
+import { LocationContext } from './LocationProvider'
+import { useParams } from 'react-router-dom'
+import './Locations.css'
+
+export const LocationDetail = () => {
+  const { getLocationById } = useContext(LocationContext)
+  
+  const [location, setLocations] = useState({})
+  
+  const {locationId} = useParams()
+  
+  useEffect(()=> {
+    getLocationById(locationId)
+      .then(res => {
+        setLocations(res)
+      })
+  },[])// eslint-disable-line react-hooks/exhaustive-deps
+  
+ 
+  return(
+    
+    <section className="location">
+    <h3 className="location__name">
+     {location.name}
+    </h3>
+    <div className="location__address">
+      {location.address}
+    </div>
+    <h3 className="location__employee">
+     Employees
+    </h3>
+    <div className="location__employees">
+      { location.employees?.map(e => e.name ).join(", ")}
+    </div>
+    <h3 className="location__animal">
+     Animals
+    </h3>
+    <div className="location__animals">
+      { location.animals?.map(a => a.name ).join(", ")}
+    </div>
+  
+  </section>
+   
+  )
+}
